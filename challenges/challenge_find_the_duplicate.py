d492fc76338a8c7066105632befbd4ea0de87371
Mixchelle <michelle.biologia@hotmail.com>
2023-06-02 13:32:46 -0300

    requisito 05 e 06

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,13 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(nums) <= 1 or isinstance(nums, str):
+        return False
+
+    num_set = set()
+    for num in nums:
+        if isinstance(num, str) or num < 0:
+            return False
+        if num in num_set:
+            return num
+        num_set.add(num)
+
+    return False
