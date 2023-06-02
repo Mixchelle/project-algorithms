7da264a778876a14cfd5b12f18bcad231e941f59
Mixchelle <michelle.biologia@hotmail.com>
2023-06-02 14:00:35 -0300

    fix linter

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,32 +1,17 @@
-def count_study(persistence_period, target_time):
+def study_schedule(permanence_period, target_time):
+    if not isinstance(target_time, int):
+        return None
+
     count = 0
-    for period in persistence_period:
-        start_time, end_time = period
+    for start_time, end_time in permanence_period:
+        if not isinstance(start_time, int) or not isinstance(end_time, int):
+            return None
+
+        if start_time > end_time:
+            return None
 
         if start_time <= target_time <= end_time:
             count += 1
 
     return count
 
-
-def is_valid_entry(entry):
-    if len(entry) != 2:
-        return False
-
-    start_time, end_time = entry
-    if not isinstance(start_time, int) or not isinstance(end_time, int):
-        return False
-
-    if start_time > end_time:
-        return False
-    return True
-
-
-def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    if not isinstance(target_time, int):
-        return None
-
-    if not all(is_valid_entry(entry) for entry in permanence_period):
-        return None
-    return count_study(permanence_period, target_time)
