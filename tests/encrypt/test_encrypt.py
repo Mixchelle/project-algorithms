528c6516d01baed0a76e3f2cf84a587413905403
Mixchelle <michelle.biologia@hotmail.com>
2023-06-02 12:28:28 -0300

    Requisito 02 - Teste

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,18 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message('message', 'key')
+
+    with pytest.raises(TypeError):
+        encrypt_message(7, 20)
+
+    assert encrypt_message("message", 7) == "egassem"
+
+    assert encrypt_message("message", 3) == "sem_egas"
+
+    assert encrypt_message("message", 4) == "ega_ssem"
+
+
